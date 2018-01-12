af553ebc8f8cc7130b174345f230a1025f8ef268
biqing.hu <biqing.hu@zhaopin.com.cn>
2018-01-12 16:05:29 +0800

    option

---
+++ src/pages/home/common/select/option.vue
@@ -0,0 +1,42 @@
+<template>  
+     <IViewOption class="zpfe-iview-option"
+     :value="value"
+     :label="label"
+     :disabled="disabled"
+     >
+        <slot></slot>
+     </IViewOption>
+</template>
+
+<script> 
+import IViewOption from 'iview/src/components/option'
+export default {
+    components: { 
+        IViewOption
+    },
+    props: {
+        value: {
+            type: [String, Number],
+            required: true
+        },
+        label: {
+            type: [String, Number]
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data() {
+
+    },
+
+    methods: {
+
+    }
+}
+</script>
+<style lang="scss">
+.zpfe-iview-option {
+}
+</style>
